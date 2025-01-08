7581079889cde76883081e648ff1ed318807f880
telkomdev-arung <akuarung83@gmail.com>
2025-01-08 11:08:51 +0700

    init

--- index.py
+++ index.py
@@ -1,10 +1,45 @@
-import os
+import numpy as np
 
-def list_files_in_root():
-    root_directory = '/'
-    files = os.listdir(root_directory)
-    for file in files:
-        print(file)
+class SVM:
+    def __init__(self, learning_rate=0.001, lambda_param=0.01, n_iters=1000):
+        self.learning_rate = learning_rate
+        self.lambda_param = lambda_param
+        self.n_iters = n_iters
+        self.w = None
+        self.b = None
 
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        y_ = np.where(y <= 0, -1, 1)
+        self.w = np.zeros(n_features)
+        self.b = 0
+
+        for _ in range(self.n_iters):
+            for idx, x_i in enumerate(X):
+                condition = y_[idx] * (np.dot(x_i, self.w) - self.b) >= 1
+                if condition:
+                    self.w -= self.learning_rate * (2 * self.lambda_param * self.w)
+                else:
+                    self.w -= self.learning_rate * (2 * self.lambda_param * self.w - np.dot(x_i, y_[idx]))
+                    self.b -= self.learning_rate * y_[idx]
+
+    def predict(self, X):
+        approx = np.dot(X, self.w) - self.b
+        return np.sign(approx)
+
+def accuracy(y_true, y_pred):
+    accuracy = np.sum(y_true == y_pred) / len(y_true)
+    return accuracy
+
+# Example usage
 if __name__ == "__main__":
-    list_files_in_root()
+
+    # Sample data
+    X = np.array([[1, 2], [2, 3], [3, 4], [4, 5], [1, 0], [0, 1], [2, 2], [3, 3]])
+    y = np.array([1, 1, 1, 1, -1, -1, -1, -1])
+
+    clf = SVM()
+    clf.fit(X, y)
+    predictions = clf.predict(X)
+
+    print("SVM classification accuracy:", accuracy(y, predictions))
